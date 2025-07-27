333bda5ab4442825bdc5346d452a17b88eaa9cd3
Hilina Teshome <teshilina8@gmail.com>
2025-07-27 18:36:27 +0300

    models file updated

--- advanced_features_and_security/advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/advanced_features_and_security/bookshelf/models.py
@@ -2,6 +2,15 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} by {self.author}" 
+
+
 
 # Custom Manager
 class CustomUserManager(BaseUserManager):
@@ -39,13 +48,5 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.title} by {self.author}" 
 
 
