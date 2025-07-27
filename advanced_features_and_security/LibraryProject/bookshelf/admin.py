f777ce83a777a46df71145db18c43a45499dd9cc
Hilina Teshome <teshilina8@gmail.com>
2025-07-27 18:48:52 +0300

    Task o completed

---
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Book ,CustomUser
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display=('title', 'author', 'publication_year')
+    list_filter = ('publication_year', 'author')
+    search_fields = ('title','author')
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'profile_photo'),
+        }),
+    )
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
---
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    genre = models.CharField(max_length=100, null=True, blank = True)
+    published_date = models.DateField(default=timezone.now)
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
