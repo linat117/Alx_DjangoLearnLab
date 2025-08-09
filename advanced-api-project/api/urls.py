f067cc2dc10c8bd2d9233535258c317d592703b7
Hilina Teshome <teshilina8@gmail.com>
2025-08-09 16:22:21 +0300

    Complete task 1 and fixed issue

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -19,8 +19,8 @@ urlpatterns = [
     path('books/create/', BookCreateView.as_view(), name='book-create'),
 
     # Update a book (PUT / PATCH) - requires authentication
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),
 
     # Delete a book (DELETE) - requires authentication
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
