3efe4028e56b3027be911d1a2834c56ba32447de
Hilina Teshome <teshilina8@gmail.com>
2025-08-12 20:33:40 +0300

    install taggit and add to settings.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from taggit.managers import TaggableManager
 from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length = 200)
