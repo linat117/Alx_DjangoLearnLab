eb6a88960036f96a52cbb754809923d90f10eaab
Hilina Teshome <teshilina8@gmail.com>
2025-08-12 17:46:44 +0300

    Create Views, forms and urls file

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
+class RegisterView(View):
+    def get(self, request):
+        form = CustomUserCreationForm()
+        return render(request, 'register.html', {'form': form})
+    
+    def post(self, request):
+        form  = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+        return render(request, 'register.html', {'form' : form})
